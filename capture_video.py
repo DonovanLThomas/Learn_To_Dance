cdf69ea3d5bb3c3214e3fa0972868cc2cdddb62b
DonoTech <dlthomas2005@gmail.com>
2026-05-22 22:17:19 -0700

    fixed videocaptue uses the ast_argus correctly

--- capture_video.py
+++ capture_video.py
@@ -1,17 +1,44 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=6,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, "
+        "framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
 
 while(True):
     ret, frame = cap.read()
 
-    flipped_frame = cv2.flip(frame, 0)
-    gray = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('frame', gray)
 
-    if cv2.waitkey(1) & '0xFF' == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
